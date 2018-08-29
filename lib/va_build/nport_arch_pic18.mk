b609b255bafbc1a0621b001042ae4952f4a4079a
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2018-08-29 14:05:13 +0200

    arch_pic18: Added include path.
    Signed-off-by: Nenad Radulovic <nenad.b.radulovic@gmail.com>

--- lib/va_build/nport_arch_pic18.mk
+++ lib/va_build/nport_arch_pic18.mk
@@ -21,3 +21,6 @@ BUILD_ARCH_DESC = "Microchip PIC18 arhitecture"
 
 # Define platform for this architecture
 PLATFORM := xc8
+
+# Includes and sources
+CC_INCLUDES += lib/va_include/nport/arch_pic18
