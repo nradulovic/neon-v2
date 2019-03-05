aafd2ec3b3c3b9db0ec23374875f573387ce62c4
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2019-03-05 01:21:45 +0100

    Added OS none variant
    Signed-off-by: Nenad Radulovic <nenad.b.radulovic@gmail.com>

---
+++ variant/os/none/os_variant/os.h
@@ -0,0 +1,46 @@
+/*
+ * Neon
+ * Copyright (C)  2018  REAL-TIME CONSULTING
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+/** @file
+ *  @author      Nenad Radulovic
+ *  @brief       Variant architecture for x86 header
+ *
+ *  @addtogroup  port
+ *  @{
+ */
+/** @defgroup    port_x86_variant_arch Variant architecture for x86
+ *  @brief       Variant architecture for x86.
+ *  @{
+ */
+/*---------------------------------------------------------------------------*/
+
+
+#ifndef NEON_NONE_OS_VARIANT_OS_H_
+#define NEON_NONE_OS_VARIANT_OS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+/** @} */
+/** @} */
+/*---------------------------------------------------------------------------*/
+#endif /* NEON_NONE_OS_VARIANT_OS_H_ */
