bc5df96610ecb65241b88ef476b5001fb2100c63
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2019-08-28 09:54:07 +0200

    Added variant parts for generic_board and linux_os.
    Signed-off-by: Nenad Radulovic <nenad.b.radulovic@gmail.com>

---
+++ variant/board/generic/board_variant/generic_board.c
@@ -0,0 +1,13 @@
+/*
+ * generic_board.c
+ *
+ *  Created on: Aug 27, 2019
+ *      Author: nenad
+ */
+
+#include "sys/nport.h"
+
+void nboard_init(void)
+{
+
+}
---
+++ variant/os/linux/linux_os.c
@@ -0,0 +1,10 @@
+/*
+ * linux_os.c
+ *
+ *  Created on: Aug 27, 2019
+ *      Author: nenad
+ */
+
+#include "os_variant/os.h"
+
+pthread_mutex_t g_nglobal_mutex = PTHREAD_MUTEX_INITIALIZER;
