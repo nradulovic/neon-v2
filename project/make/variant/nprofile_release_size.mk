66714edbf23282c92e1ba00036cb912931a681e4
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2020-02-04 12:11:46 +0100

    project/make: Updated headers of makefiles in variant folder.
    Signed-off-by: Nenad Radulovic <nenad.b.radulovic@gmail.com>

--- project/make/variant/nprofile_release_size.mk
+++ project/make/variant/nprofile_release_size.mk
@@ -2,6 +2,14 @@
 # Neon
 # Copyright (C) 2018   REAL-TIME CONSULTING
 #
+#
+# Neon
+# Copyright (C) 2018   REAL-TIME CONSULTING
+#
+#
+# Neon
+# Copyright (C) 2018   REAL-TIME CONSULTING
+#
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option)
